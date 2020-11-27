05b5bacc4eeac8cfe2f5116e18afc3a7c4d13ca9
Dan Mano <danmano411@gmail.com>
2020-11-27 16:34:48 -0500

    made slingshot for bird

--- Slingshot.js
+++ Slingshot.js
@@ -3,11 +3,14 @@ class SlingShot{
         var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 0.04,
+            stiffness: 0.08,
             length: 10
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.image1 = loadImage("sprites/sling1.png");
+        this.image2 = loadImage("sprites/sling2.png")
+        this.image3 = loadImage("sprites/sling3.png")
         World.add(world, this.sling);
     }
 
@@ -19,9 +22,24 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            push();
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            //line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48, 22, 8);
+            if (pointA.x < 220){
+                line(pointA.x - 20, pointA.y, pointB.x - 20, pointB.y)
+                line(pointA.x - 20, pointA.y, pointB.x + 20, pointB.y)
+                image(this.image3, pointA.x - 25, pointA.y - 10, 15, 30)
+
+            } else {
+                line(pointA.x + 20, pointA.y, pointB.x + 20, pointB.y)
+                line(pointA.x + 20, pointA.y, pointB.x - 20, pointB.y)
+                image(this.image3, pointA.x + 25, pointA.y - 10, 15, 30)
+            }
+            pop();
         }
+        image(this.image1, 200, 25)
+        image(this.image2, 170, 20)
     }
     
 }
